0db4185933a03de48478160c004753af9ae2bcb4
BlackT1221 <cdmartinez1121@gmail.com>
2024-03-11 16:29:23 -0500

    Juego terminado

--- Index.js
+++ Index.js
@@ -1,9 +1,35 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+    let flippedCards = [];
+    let text = document.getElementById("textWinner")
     for (let i = 1; i <= 8; i++) {
         const card = document.getElementById(`card${i}`);
         if (card) {
             card.addEventListener('click', () => {
-                card.classList.toggle('flipped');
+                if (flippedCards.length < 2) {
+                    card.classList.toggle('flipped');
+                    flippedCards.push(card);
+                }
+                if (flippedCards.length === 2) {
+                    // Obtenemos el texto de la cara trasera de las cartas
+                    let textoCarta1 = flippedCards[0].querySelector('.back p').textContent;
+                    let textoCarta2 = flippedCards[1].querySelector('.back p').textContent;
+
+                    // Comparamos si los textos son iguales
+                    if (textoCarta1 === textoCarta2) {
+                        alert('¡Has encontrado un par!');
+                        flippedCards.forEach(card => card.classList.add('removed'));
+                    }
+                    setTimeout(() => {
+                        flippedCards.forEach(card => card.classList.remove('flipped'));
+                        flippedCards = [];
+                    }, 1000);
+
+                    // Verificamos si todas las cartas tienen la clase 'removed'
+                    let todasLasCartas = document.querySelectorAll('.card');
+                    if (Array.from(todasLasCartas).every(card => card.classList.contains('removed'))) {
+                        text.classList.remove('removed');
+                    }
+                }
             });
         }
     }
